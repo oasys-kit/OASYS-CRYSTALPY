ea6d227dc7c25cabcab59a266a7cf75c2eb7cc17
Manuel Sanchez del Rio <srio@esrf.eu>
2016-12-22 11:01:39 +0100

    declare workspace

--- orangecontrib/oasyscrystalpy/widgets/__init__.py
+++ orangecontrib/oasyscrystalpy/widgets/__init__.py
@@ -1,6 +1,10 @@
 """
 CrystalPy
 """
+
+# namespace declaration.
+__import__("pkg_resources").declare_namespace(__name__)
+
 NAME = "CrystalPy"
 
 DESCRIPTION = "Analysis of the polarization and crystal diffraction of x-rays."
