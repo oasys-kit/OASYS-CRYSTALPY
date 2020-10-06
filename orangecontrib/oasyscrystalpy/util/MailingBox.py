9de691026edafeb67f308681e307badc588aa273
Manuel Sanchez del Rio <srio@esrf.eu>
2020-10-06 16:59:41 +0200

    cleaning + updating

--- orangecontrib/oasyscrystalpy/util/MailingBox.py
+++
@@ -1,9 +0,0 @@
-"""
-Contains the data that are sent by the crystal diffraction widget to the viewer.
-"""
-
-
-class MailingBox(object):
-    def __init__(self, diffraction_result, mueller_result):
-        self.diffraction_result = diffraction_result
-        self.mueller_result = mueller_result
--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 #! /usr/bin/env python3
 
-import imp
 import os
-import sys
-import subprocess
 
-NAME = 'oasyscrystalpy'
+try:
+    from setuptools import find_packages, setup
+except AttributeError:
+    from setuptools import find_packages, setup
 
-VERSION = '1.0'
+NAME = 'OASYS1-CRYSTALPY'
+
+VERSION = '1.1'
 ISRELEASED = False
 
-DESCRIPTION = 'oasyscrystalpy:  Oasys interface for crystalpy'
+DESCRIPTION = 'Oasys interface for crystalpy'
 README_FILE = os.path.join(os.path.dirname(__file__), 'README.txt')
 LONG_DESCRIPTION = open(README_FILE).read()
-AUTHOR = 'Edoardo Cappelli'
-AUTHOR_EMAIL = 'cappelli@esrf.fr'
-URL = 'http://orange.biolab.si/'
-DOWNLOAD_URL = 'http://github.com/edocappelli/oasys-crystalpy'
+AUTHOR = 'Edoardo Cappelli, Mark Glass and Manuel Sanchez del Rio'
+AUTHOR_EMAIL = 'srio@esrf.eu'
+URL = 'https://github.com/oasys-kit/OASYS-CRYSTALPY'
+DOWNLOAD_URL = 'https://github.com/oasys-kit/OASYS-CRYSTALPY'
 LICENSE = 'MIT'
 
 KEYWORDS = (
@@ -44,66 +46,52 @@ SETUP_REQUIRES = (
                   )
 
 INSTALL_REQUIRES = (
-                    'setuptools',
-                    'numpy',
+                    'oasys1',
                     'crystalpy',
-                    'matplotlib'
                    )
 
-if len({'develop', 'release', 'bdist_egg', 'bdist_rpm', 'bdist_wininst',
-        'install_egg_info', 'build_sphinx', 'egg_info', 'easy_install',
-        'upload', 'test'}.intersection(sys.argv)) > 0:
-    import setuptools
-    extra_setuptools_args = dict(
-        zip_safe=False,  # the package can run out of an .egg file
-        include_package_data=True,
-        install_requires=INSTALL_REQUIRES
-    )
-else:
-    extra_setuptools_args = dict()
 
-from setuptools import find_packages, setup
+PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
 
-PACKAGES = find_packages(
-                         exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'),
-                         )
-
-PACKAGE_DATA = {"orangecontrib.oasyscrystalpy.widgets.elements": ["icons/*.png", "icons/*.jpg"],
-            }
+PACKAGE_DATA = {
+    "orangecontrib.crystalpy.widgets.elements": ["icons/*.png", "icons/*.jpg"],
+}
 
 
-NAMESPACE_PACKAGES = ["orangecontrib","orangecontrib.oasyscrystalpy", "orangecontrib.oasyscrystalpy.widgets"]
+NAMESPACE_PACKAGES = ["orangecontrib", "orangecontrib.crystalpy", "orangecontrib.crystalpy.widgets"]
 
 
 ENTRY_POINTS = {
-    'oasys.addons': ("CrystalPy = orangecontrib.oasyscrystalpy", ),
+    'oasys.addons': ("CrystalPy = orangecontrib.crystalpy", ),
     'oasys.widgets': (
-        "CrystalPy = orangecontrib.oasyscrystalpy.widgets.elements",
+        "CrystalPy = orangecontrib.crystalpy.widgets.elements",
     ),
 }
 
 if __name__ == '__main__':
-    setup(
-          name=NAME,
-          version=VERSION,
-          description=DESCRIPTION,
-          long_description=LONG_DESCRIPTION,
-          author=AUTHOR,
-          author_email=AUTHOR_EMAIL,
-          url=URL,
-          download_url=DOWNLOAD_URL,
-          license=LICENSE,
-          keywords=KEYWORDS,
-          classifiers=CLASSIFIERS,
-          packages=PACKAGES,
-          package_data=PACKAGE_DATA,
-          # py_modules=PY_MODULES,
-          setup_requires=SETUP_REQUIRES,
-          install_requires=INSTALL_REQUIRES,
-          # extras_require=EXTRAS_REQUIRE,
-          # dependency_links=DEPENDENCY_LINKS,
-          entry_points=ENTRY_POINTS,
-          namespace_packages=NAMESPACE_PACKAGES,
-          include_package_data=True,
-          zip_safe=False,
-          )
+    try:
+        import PyMca5, PyQt4
+
+        raise NotImplementedError("This version of SRW doesn't work with Oasys1 beta.\nPlease install OASYS1 final release: https://www.aps.anl.gov/Science/Scientific-Software/OASYS")
+    except:
+        setup(
+              name=NAME,
+              version=VERSION,
+              description=DESCRIPTION,
+              long_description=LONG_DESCRIPTION,
+              author=AUTHOR,
+              author_email=AUTHOR_EMAIL,
+              url=URL,
+              download_url=DOWNLOAD_URL,
+              license=LICENSE,
+              keywords=KEYWORDS,
+              classifiers=CLASSIFIERS,
+              packages=PACKAGES,
+              package_data=PACKAGE_DATA,
+              setup_requires=SETUP_REQUIRES,
+              install_requires=INSTALL_REQUIRES,
+              entry_points=ENTRY_POINTS,
+              namespace_packages=NAMESPACE_PACKAGES,
+              include_package_data=True,
+              zip_safe=False,
+              )
